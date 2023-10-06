daaba659df40ef5650a71cb7de08adddfcf545df
dkause <d.g.kause@gmail.com>
2023-10-06 16:59:47 +0200

    UPDATE Remove Movies Array

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,57 +3,7 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-
-    {
-      id: 1,
-
-      Title: "Silence of the Lambs",
-
-      Description: "To catch a serial killer, a yound FBI cadet seeks the help of an incarcerated and manipulative cannibal killer.",
-
-      Genre: "Thriller",
-
-      Director: "Jonathan Demme",
-
-      ImagePath: "https://placehold.co/600x400",
-
-      Featured: true
-    },
-
-    {
-      id: 2,
-
-      Title: "Star Wars: Episode IV - A New Hope",
-
-      Description: "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empire's world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader.",
-
-      Genre: "Fantasy",
-
-      Director: "George Lucas",
-
-      ImagePath: "https://placehold.co/600x400",
-
-      Featured: true
-    },
-
-    {
-      id: 3,
-
-      Title: "Star Wars: Episode X – The Rose of Skywalker",
-
-      Description: "In the riveting conclusion of the landmark Skywalker saga, Luke finally finds his peace in becoming a gardrner.",
-
-      Genre: "Fantasy",
-
-      Director: "J.J. Abrams",
-
-      ImagePath: "https://placehold.co/600x400",
-
-      Featured: true
-    },
-
-  ]);
+  const [movies, setMovies] = useState([ ]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
