ebe7a189732437fb0a30371576acaf7b07fe9ba3
dkause <d.g.kause@gmail.com>
2023-10-19 17:29:13 +0200

    ADD React-bootstrap

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,24 +1,31 @@
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
+import { Button, Card } from 'react-bootstrap'
 
 export const MovieCard = ({ movie, onMovieClick }) => {
-  console.log("MovieCard props:", { movie, onMovieClick });
+  console.log('MovieCard props:', { movie, onMovieClick })
   if (!movie) {
-    return null; // or some other fallback UI
+    return null // or some other fallback UI
   }
   return (
-    <div
-      onClick={() => {
-        onMovieClick(movie);
-      }}
-    >
-      {movie.Title}
-    </div>
-  );
-};
+    <Card>
+      <Card.Img variant='top' src={movie.ImagePath} />
+      <Card.Body>
+        <Card.Title>{movie.Title}</Card.Title>
+        <Button
+          onClick={() => {
+            onMovieClick(movie)
+          }}
+        >
+        </Button>
+      </Card.Body>
+    </Card>
+  )
+}
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    Title: PropTypes.string.isRequired
+    Title: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired
   }),
   onMovieClick: PropTypes.func.isRequired
-};
+}
